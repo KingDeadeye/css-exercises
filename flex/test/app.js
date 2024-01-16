168f0b7c4ef2aa0389f75d74199b46de4e95669b
KingDeadeye <bhiserutuj@gmail.com>
2024-01-16 12:45:57 +0530

    some final changes

---
+++ flex/test/app.js
@@ -0,0 +1,25 @@
+// function add7(x) {
+//     x=x+7;
+//     return x;
+// }
+
+// add7(3);
+
+// const multiply=(a,b) => a*b;
+// console.log(multiply(2,3));
+
+
+// function capitalize(x){
+//     x =x.toUpperCase();
+//     return x.at(0);
+// }
+
+// console.log(capitalize("HELO"));
+    
+
+function lastLetter(x) {
+
+    return x.at(-1)
+    
+}
+console.log(lastLetter("HELo"));
